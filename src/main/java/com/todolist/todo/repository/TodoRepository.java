5dc8a67242f8e467026768717dd3d3117b42d48c
luongtan204 <luongtan204@gmail.com>
2026-07-06 02:38:59 +0700

    add filtering functionality to getTodos method in TodoService and TodoRepository

--- src/main/java/com/todolist/todo/repository/TodoRepository.java
+++ src/main/java/com/todolist/todo/repository/TodoRepository.java
@@ -1,9 +1,27 @@
 package com.todolist.todo.repository;
 
 import com.todolist.todo.entity.Todo;
+import com.todolist.todo.entity.TodoStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+	@Query("""
+			select t from Todo t
+			where (:status is null or t.status = :status)
+			  and (
+					:keyword is null or :keyword = ''
+					or lower(t.title) like lower(concat('%', :keyword, '%'))
+					or lower(coalesce(t.description, '')) like lower(concat('%', :keyword, '%'))
+				  )
+			""")
+	Page<Todo> findByFilters(@Param("status") TodoStatus status,
+						   @Param("keyword") String keyword,
+						   Pageable pageable);
 }
 
